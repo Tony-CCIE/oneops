0b6970700a85aadb906012d4441911cd1e9cdcf9
Cai <Gupeng.Cai@connext.com.cn>
2020-09-11 09:59:09 +0800

    add

--- Lib/zabbix_info.py
+++ Lib/zabbix_info.py
@@ -1,30 +1,151 @@
-from fastapi import FastAPI
 import requests
 import json
-from Lib import login_token
 
+
+# 登录，获取 token 值
 url = 'http://10.157.27.56/zabbix/api_jsonrpc.php'
 post_headers = {'Content-Type': 'application/json'}
 post_data = {
     "jsonrpc": "2.0",
-    "method": "host.get",
+    "method": "user.login",
     "params": {
-        "output": [
-            "host",
-        ],
-        "selectInterfaces": [
-            "ip"
-        ]
+        "user": "shenping",
+        "password": "password@123"
     },
-    "id": 2,
-    "auth": login_token.token
+    "id": 1
 }
+
 r = requests.post(url, data=json.dumps(post_data), headers=post_headers)
-# print(r.text)
 r = json.loads(r.text)
-r_result = r['result']
-for index, item in enumerate(r_result):
-    print(item['host'])
+token = r["result"]
+
+# 所有已配置主机的ID，主机名和接口
+def host_ip():
+    url = 'http://10.157.27.56/zabbix/api_jsonrpc.php'
+    post_headers = {'Content-Type': 'application/json'}
+    post_data = {
+        "jsonrpc": "2.0",
+        "method": "host.get",
+        "params": {
+            "output": ["hostid", "host"],
+            "selectInterfaces": ["ip"]
+        },
+        "id": 1,
+        "auth": token
+    }
+    r = requests.post(url, data=json.dumps(post_data), headers=post_headers)
+    r = json.loads(r.text)
+    r_result = r['result']
+
+    hostip_list = []
+    for index, item in enumerate(r_result):
+        hostip_list.append(item)
+    return hostip_list
+
+# system.cpu.util[,idle] 每分钟 CPU 使用率
+# vm.memory.size[available]  使用内存（单位为Bit)
+# net.if.in[eth0] bps 网口 in 流量
+# net.if.out[eth0] 网口 out 流量
+
+def cpu_usage(hostid):
+    url = 'http://10.157.27.56/zabbix/api_jsonrpc.php'
+    post_headers = {'Content-Type': 'application/json'}
+    post_data = {
+        "jsonrpc": "2.0",
+        "method": "item.get",
+        "params": {
+            "output": "extend",
+            "hostids": hostid,
+            "search": {
+                "key_": "system.cpu.util[,idle]"
+            }
+        },
+        "id": 1,
+        "auth": token
+    }
+    r = requests.post(url, data=json.dumps(post_data), headers=post_headers)
+    r = json.loads(r.text)
+    r_result = r['result']
+    if len(r_result):
+        return (r_result[0]['lastvalue'])
+    else:
+        return ("result: []")
 
 
+def memory_usage(hostid):
+    url = 'http://10.157.27.56/zabbix/api_jsonrpc.php'
+    post_headers = {'Content-Type': 'application/json'}
+    post_data = {
+        "jsonrpc": "2.0",
+        "method": "item.get",
+        "params": {
+            "output": "extend",
+            "hostids": hostid,
+            "search": {
+                "key_": "vm.memory.size[available]"
+            }
+        },
+        "id": 1,
+        "auth": token
+    }
+    r = requests.post(url, data=json.dumps(post_data), headers=post_headers)
+    r = json.loads(r.text)
+    r_result = r['result']
+    if len(r_result):
+        return (r_result[0]['lastvalue'])
+    else:
+        return "result:[]"
 
+
+def net_in(hostid):
+    url = 'http://10.157.27.56/zabbix/api_jsonrpc.php'
+    post_headers = {'Content-Type': 'application/json'}
+    post_data = {
+        "jsonrpc": "2.0",
+        "method": "item.get",
+        "params": {
+            "output": "extend",
+            "hostids": "10169",
+            "search": {
+                "key_": ["net.if.in[eth0]"]
+            }
+        },
+        "id": 1,
+        "auth": token
+    }
+    r = requests.post(url, data=json.dumps(post_data), headers=post_headers)
+    r = json.loads(r.text)
+    r_result = r['result']
+    if len(r_result):
+        return (r_result[0]['lastvalue'])
+    else:
+        return "result:[]"
+
+
+# def net_out():
+#     url = 'http://10.157.27.56/zabbix/api_jsonrpc.php'
+#     post_headers = {'Content-Type': 'application/json'}
+#     post_data = {
+#         "jsonrpc": "2.0",
+#         "method": "item.get",
+#         "params": {
+#             "output": "extend",
+#             "hostids": hostid,
+#             "search": {
+#                 "key_": "net.if.in[eth0]"
+#             }
+#         },
+#         "id": 1,
+#         "auth": token
+#     }
+#     r = requests.post(url, data=json.dumps(post_data), headers=post_headers)
+#     r = json.loads(r.text)
+#     r_result = r['result']
+#     return (r_result[0]['lastvalue'])
+
+if __name__ == "__main__":
+   # cpu_usage(),\
+   # memory_usage(),\
+   # ping(),
+   # host_ip()
+    net_in()
--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from Lib import zabbix_info, redis_info
 
 app = FastAPI()
-
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/zbbix/host/list")
+def host_list():
+    return zabbix_info.host_ip()
+
+@app.get("/zbbix/{hostid}/cpu/")
+def cpu_uasge(hostid: int):
+    return zabbix_info.cpu_usage(hostid)
+
+@app.get("/zbbix/{hostid}/memory/")
+def memory_uasge(hostid: int):
+    return zabbix_info.memory_usage(hostid)
+
+@app.get("/zbbix/{hostid}/net_in/")
+def memory_uasge(hostid: int):
+    return zabbix_info.memory_usage(hostid)
+
+@app.get("/zbbix/{hostid}/memory/")
+def memory_uasge(hostid: int):
+    return zabbix_info.memory_usage(hostid)
+
+@app.get("/redis/slowlog")
+def slowlog():
+    return redis_info.slowlog_get()
