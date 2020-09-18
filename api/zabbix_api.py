c9d980b356dc3647c2e4a8e2a79c1b8708a392f8
Tony-CCIE <1278146756@qq.com>
2020-09-18 14:07:25 +0800

    dict sort

--- api/zabbix_api.py
+++ api/zabbix_api.py
@@ -14,6 +14,12 @@ class Zabbix:
         data = self.zb.host.get(output=["hostid", "host"], selectInterfaces=["ip"])
         return data
 
+    def id_list(self):
+        id_list = []
+        for i in range(len(self.hosts_all())):
+            id_list.append(self.hosts_all()[i]["hostid"])
+        return id_list
+
     def host(self, host_id):
         data = self.zb.host.get(output=["host"], hostids=host_id)
         return data[0]['host']
@@ -25,39 +31,40 @@ class Zabbix:
     def cpu_idle(self, host_id):
         data = self.zb.item.get(output="extend", hostids=host_id, search={"key_": "system.cpu.util[,idle]"})
         if len(data):
-            return (data[0]["lastvalue"] + ' %')
+            return (data[0]["lastvalue"])
         else:
-            return data
+            return "null"
 
     def memory_usage(self, host_id):
         data = self.zb.item.get(output="extend", hostids=host_id, search={"key_": "vm.memory.size[available]"})
         if len(data):
             return ('{:.2f} GB'.format(float(data[0]["lastvalue"]) / 1024 / 1024 / 1024))
         else:
-            return data
+            return "null"
 
     def net_in(self, host_id):
         data = self.zb.item.get(output="extend", hostids=host_id, search={"key_": "net.if.in[eth0]"})
         if len(data):
             return ('{:.2f} Kbps'.format(float(data[0]["lastvalue"]) / 1000))
         else:
-            return data
+            return "null"
 
     def net_out(self, host_id):
         data = self.zb.item.get(output="extend", hostids=host_id, search={"key_": "net.if.out[eth0]"})
         if len(data):
             return ('{:.2f} Kbps'.format(float(data[0]["lastvalue"]) / 1000))
         else:
-            return data
+            return "null"
 
 
-# if __name__ == "__main__":
-#     z1 = Zabbix()
-#     print(z1.hosts_all())
-#     print(z1.host())
-#     print(z1.ip())
-#     print(z1.cpu_usage())
-#     print(z1.memory_usage())
-#     print(z1.net_in())
-#     print(z1.net_out())
+if __name__ == "__main__":
+    z1 = Zabbix(url='http://10.157.27.56/zabbix/api_jsonrpc.php', user="shenping", password="password@123")
+    # print(z1.hosts_all())
+    # print(z1.hostid())
+    # print((z1.id_list()))
+    # print(z1.ip())
+    print(z1.cpu_idle("10128"))
+    # print(z1.memory_usage())
+    # print(z1.net_in())
+    # print(z1.net_out())
 
--- resources/zabbix_data.py
+++ resources/zabbix_data.py
@@ -21,3 +21,21 @@ def host_info(host_id):
             "net_in": zb.net_in(host_id), "net_out": zb.net_out(host_id)
             }
     return info
+
+
+@router.get("/zabbix/ip_cpu/", tags=["zabbix"])
+def ip_cpu():
+    ip_list = []
+    cpu_list = []
+    for i in zb.id_list():
+        ip_list.append(zb.ip(i))
+        cpu_list.append(zb.cpu_idle(i))
+    ip_cpu_dict = dict(zip(ip_list, cpu_list))
+    ip_cpu_sort = dict_sort(ip_cpu_dict, 5)
+    return ip_cpu_sort
+
+
+def dict_sort(dict_n, n):
+    dict_not_null = dict((key, value) for key, value in dict_n.items() if value != "null")
+    dict_value_sort = dict(sorted(dict_not_null.items(), key=lambda x: x[1], reverse=True)[:n])
+    return dict_value_sort
